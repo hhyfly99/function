5aec884d9dce4f215028ee72b75f1aaa50807536
hhyfly99 <hhyfly99@gmail.com>
2012-08-08 13:59:25 +0800

    update

--- encrypt.php
+++ encrypt.php
@@ -1,13 +1,16 @@
 <?php
-function md5_encrypt($head, $str, $tail)
+$head = "COM3";
+$tail = "yond";
+function md5_encrypt($str)
 {
-	$head = "COM3";
-	$tail = "yond";
-	$encrypt = $head.$str.$tail;
-	echo md5($head, true);
+	global $head;
+	global $tail;
+	$encrypt_str = $head.$str.$tail;
+	//echo md5($head, true);
+	echo md5($encrypt_str);
 	echo "<br />";
-	echo md5($head);
+	echo md5($str);
 }
 
-md5_encrypt($head, "dd", $tail);
+md5_encrypt("ddd");
 ?>
